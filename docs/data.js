35bf9e7d75b4e41ba46f732ff511d275e6d9d08d
Karyn Herrera <karynherrera@gmail.com>
2018-07-05 14:55:29 -0400

    se crea carpeta docs

--- docs/data.js
+++ docs/data.js
@@ -1,16 +1,12 @@
 var provider = new firebase.auth.FacebookAuthProvider();
 firebase.auth().signInWithRedirect(provider);
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    window.logIn();
-    console.log("sip");
-    // ...
-  }
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
   window.logIn();
+  // ...
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
@@ -20,4 +16,5 @@ firebase.auth().getRedirectResult().then(function(result) {
   // The firebase.auth.AuthCredential type that was used.
   var credential = error.credential;
   // ...
+  window.logIn();
 });
