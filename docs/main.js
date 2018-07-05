a80010818f5e771c24c7b936aa180b20384c017a
Karyn Herrera <karynherrera@gmail.com>
2018-07-05 15:43:31 -0400

    se agrega login con google

--- docs/main.js
+++ docs/main.js
@@ -64,5 +64,20 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   // ...
   
 });
-});
+});// fin evento click del boton login Facebook
+
+  //login con Google
+  const logGoogle = document.getElementById("loginGm");
+  logGoogle.addEventListener('click',()=>{
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      let token = result.credential.accessToken;
+      let user = result.user;
+    }).catch(function(error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let credential = error.credential;
+    });
+  });// fin evento click del boton login Google
 
