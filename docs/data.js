9c1bb9abab60ab5b70c7ebba0e14eeb052440be4
Karyn Herrera <karynherrera@gmail.com>
2018-07-19 17:50:24 -0400

    monito friend

--- docs/data.js
+++ docs/data.js
@@ -67,12 +67,13 @@ firebase.database().ref('posts')
   <section class="enterComments" id='${newMessage.key}'>
   
   
-  <div class= "row photoUserComment"><img class="photouser" src ="icono/perfil-usuario.svg"> ${newMessage.val().creatorName} <i class="fas fa-user-plus iconFriend" onclick="window.addFriend('${userTarget}')"></i></div>
+  <div class= "row photoUserComment"><img class="photouser" src ="icono/perfil-usuario.svg"> ${newMessage.val().creatorName} <i class="fas fa-user-plus iconFriend" id="userNotFriend" onclick="window.addFriend('${userTarget}')"></i>
+  <i class="fas fa-user-check iconuserFriend" id="userFriend"></i> </div>
 
 
   <div class = "row textComment"> ${newMessage.val().text}
                
-  <i class="fas fa-user-check" id="userFriend"></i> <i class="fas fa-pencil-alt"></i> <i class="fas fa-trash" data-id="${newMessage.key}" onclick="preguntar()"></i></div> 
+  <i class="fas fa-pencil-alt"></i> <i class="fas fa-trash" data-id="${newMessage.key}" onclick="preguntar()"></i></div> 
               
 
   <row class="iconComment"><i class="fas fa-heart" data-like="${newMessage.key}" onclick="counterLike(event)"> ${newMessage.val().starCounter}<p class= "textLike">Me Gusta</p></i></div>
@@ -219,7 +220,8 @@ window.addFriend = ((userTarget) => {
       });
       if (resultFriend) {
         console.log('añadiendo amigo');
-
+        userFriend.style.display = "block";
+        userNotFriend.style.display = "none";
         const allUsersRegister = firebase.database().ref('users/');
         allUsersRegister.once('value', function(snapshot) {
           console.log('entró');
